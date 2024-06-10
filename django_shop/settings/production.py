72f85e7e139199ed6fe06eae1558a2efd92d5724
Philippe De Hovre <ph.dehovre@gmail.com>
2024-06-10 14:32:38 +0100

    Bug: Images not displaying

---
+++ django_shop/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='')
+
+STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
---
+++ django_shop/staticfiles/base/js/script.js
@@ -0,0 +1,5 @@
+console.log("helloo world")
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Document loaded')
+})
