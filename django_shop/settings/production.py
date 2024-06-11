ac98b53fa56d8a5d7d9d300e89061dfd61eaef48
philippedehovre <ph.dehovre@gmail.com>
2024-06-11 21:25:29 +0100

    Add STATIC_RUL + STATIC_ROOT to production settings

--- django_shop/settings/production.py
+++ django_shop/settings/production.py
@@ -8,12 +8,15 @@ STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/static/'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
     },
 
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
+    # "staticfiles": {
+    #     "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    # },
 }
