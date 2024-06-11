532f3234dbe58cee4ee2e2faaf47ecf845ad0569
philippedehovre <ph.dehovre@gmail.com>
2024-06-11 19:03:22 +0100

    Add allowed hosts in production settings

--- django_shop/settings/production.py
+++ django_shop/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = ['django-shop.up.railway.app']
 
 STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/static/'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
