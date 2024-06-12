6053cfeaecdf25d7c11214fa71728cd18c31484a
philippedehovre <ph.dehovre@gmail.com>
2024-06-12 17:14:46 +0100

    Another test

--- django_shop/settings/base.py
+++ django_shop/settings/base.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'fontawesomefree',
     'storages',
     'base',
     'shop',
