1fa8898b1808989f8d16a8a33575698f2888a7a3
philippedehovre <ph.dehovre@gmail.com>
2024-06-12 14:24:02 +0100

    Another test

--- django_shop/settings/production.py
+++ django_shop/settings/production.py
@@ -15,3 +15,40 @@ STORAGES = {
         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
     },
 }
+
+# from .base import *
+
+# DEBUG = True
+
+# ALLOWED_HOSTS = ['django-shop.up.railway.app','127.0.0.1', 'localhost']
+# print('DEVELOPMENT')
+
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# if config("DJANGO_ENVIRONMENT")  == "production":
+#     STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/'
+
+#     STORAGES = {
+#         "default": {
+#             "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
+#         },
+
+#         "staticfiles": {
+#             "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+#         },
+#     }
+# else :
+#     STATIC_URL= '/static/'
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#     STORAGES = {
+#         "default": {
+#             "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
+#         },
+
+#         "staticfiles": {
+#             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+#         },
+#     }
