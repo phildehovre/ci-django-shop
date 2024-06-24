febc75448b903b725e3c23f93fbb3b1e2e4d82f5
Philippe De Hovre <ph.dehovre@gmail.com>
2024-06-24 11:25:23 +0100

    Style: added responsivity to sidebar

--- django_shop/settings/development.py
+++ django_shop/settings/development.py
@@ -5,6 +5,8 @@ ALLOWED_HOSTS = ['django-shop.up.railway.app','127.0.0.1', 'localhost']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+print("ENVIRONMENT", config('DJANGO_ENVIRONMENT'))
+
 if config("DJANGO_ENVIRONMENT")  == "production":
     STATIC_URL = f'{AWS_S3_URL_PROTOCOL}//{AWS_S3_CUSTOM_DOMAIN}/'
 
@@ -24,7 +26,8 @@ else :
 
     STORAGES = {
         "default": {
-            "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
+
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
         },
 
         "staticfiles": {
