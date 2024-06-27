50b7585cd8fd531566be9a133e61f9a952a187ab
Philippe De Hovre <ph.dehovre@gmail.com>
2024-06-27 10:30:15 +0100

    Stash

--- django_shop/settings/production.py
+++ django_shop/settings/production.py
@@ -1,54 +1,39 @@
 from .base import *
 
-DEBUG = False
+ALLOWED_HOSTS = ['django-shop.up.railway.app','127.0.0.1', 'localhost']
 
-ALLOWED_HOSTS = ['django-shop.up.railway.app', '127.0.0.1', 'localhost']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AWS_LOCATION="django_shop"
 
-STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/'
+if config("DJANGO_ENVIRONMENT")  == "production":
+    # This determines the location of the files the app will source.
+    STATIC_URL = f'{AWS_S3_URL_PROTOCOL}//{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
-    },
+    # This determines where the files will be uploaded
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+            "LOCATION": AWS_LOCATION
+        },
 
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-    },
-}
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+            "LOCATION": AWS_LOCATION
+        },
+    }
 
-# from .base import *
+else :
+    STATIC_URL= '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# DEBUG = True
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+            "LOCATION": AWS_LOCATION
+        },
 
-# ALLOWED_HOSTS = ['django-shop.up.railway.app','127.0.0.1', 'localhost']
-# print('DEVELOPMENT')
-
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# if config("DJANGO_ENVIRONMENT")  == "production":
-#     STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/'
-
-#     STORAGES = {
-#         "default": {
-#             "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
-#         },
-
-#         "staticfiles": {
-#             "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-#         },
-#     }
-# else :
-#     STATIC_URL= '/static/'
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-#     STORAGES = {
-#         "default": {
-#             "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
-#         },
-
-#         "staticfiles": {
-#             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-#         },
-#     }
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
